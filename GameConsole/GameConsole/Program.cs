4e3f5ed81dfcc96dd658ae5fd2f7916edc0a2767
mohit <mohitsharmaonline+gitkraken@gmail.com>
2019-07-01 15:06:03 +0530

    setting DaysSinceLastLogin

--- GameConsole/GameConsole/Program.cs
+++ GameConsole/GameConsole/Program.cs
@@ -12,6 +12,7 @@ namespace GameConsole
         {
             var player = new PlayerCharacter();
             player.Name = "Sarah";
+            player.DaysSinceLastLogin = 42;
             PlayerDisplayer.Write(player);
 
 
