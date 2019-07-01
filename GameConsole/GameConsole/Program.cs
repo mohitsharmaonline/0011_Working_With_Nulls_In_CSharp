459595f6454a65ca3a81921d06a89bed2a2070e8
mohit <mohitsharmaonline+gitkraken@gmail.com>
2019-07-01 17:08:40 +0530

    Demo of IsNullOrWhitespace.

--- GameConsole/GameConsole/Program.cs
+++ GameConsole/GameConsole/Program.cs
@@ -11,7 +11,7 @@ namespace GameConsole
         static void Main(string[] args)
         {
             var player = new PlayerCharacter();
-            player.Name = "Sarah";
+            player.Name = null;
             //player.DaysSinceLastLogin = 42;
             PlayerDisplayer.Write(player);
 
