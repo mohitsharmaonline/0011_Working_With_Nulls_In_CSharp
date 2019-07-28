cd39108a0f080eb402e1e88877f7dcda1e59a627
Mohit Sharma <mohitsharmaonline@gmail.com>
2019-07-28 16:35:15 +0530

    Code changes to demo the use of GetValueOrDefault method of Nullable struct. Now here we are using the method overload with passed default value of -1, otherwise we could have used overload without parameter to get default value of underlying type, which would be 0.

--- GameConsole/GameConsole/PlayerDisplayer.cs
+++ GameConsole/GameConsole/PlayerDisplayer.cs
@@ -19,14 +19,17 @@ namespace GameConsole
                 Console.WriteLine(player.Name);
             }
 
-            if(player.DaysSinceLastLogin.HasValue)
-            {
-                Console.WriteLine(player.DaysSinceLastLogin.Value);                
-            }
-            else
-            {
-                Console.WriteLine("No value for DaysSinceLastLogin");
-            }
+            int days = player.DaysSinceLastLogin.GetValueOrDefault(-1);
+            Console.WriteLine($"{days} since last login.");
+
+            //if(player.DaysSinceLastLogin.HasValue)
+            //{
+            //    Console.WriteLine(player.DaysSinceLastLogin.Value);                
+            //}
+            //else
+            //{
+            //    Console.WriteLine("No value for DaysSinceLastLogin");
+            //}
 
             if(player.DateOfBirth == null)
             {
