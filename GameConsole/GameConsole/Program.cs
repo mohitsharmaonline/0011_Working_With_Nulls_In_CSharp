8b54b1ddc8ba0bce49e322c43d48d3781f205301
Mohit Sharma <mohitsharmaonline@gmail.com>
2019-07-28 17:28:09 +0530

    variation of code input data.

--- GameConsole/GameConsole/Program.cs
+++ GameConsole/GameConsole/Program.cs
@@ -10,12 +10,14 @@ namespace GameConsole
     {
         static void Main(string[] args)
         {
-            PlayerCharacter[] players = new PlayerCharacter[3]
-            {
-                new PlayerCharacter{ Name="Sarah"},
-                new PlayerCharacter(),
-                null
-            };
+            //PlayerCharacter[] players = new PlayerCharacter[3]
+            //{
+            //    new PlayerCharacter{ Name="Sarah"},
+            //    new PlayerCharacter(),
+            //    null
+            //};
+
+            PlayerCharacter[] players = null;
 
             string p1 = players?[0]?.Name;
             string p2 = players?[1]?.Name;
