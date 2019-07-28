e9c80ec8edce9f7668d489bd9045b0e76d203abd
Mohit Sharma <mohitsharmaonline@gmail.com>
2019-07-28 17:24:33 +0530

    Code for display of variation of Null conditional operator applicable on arrays.

--- GameConsole/GameConsole/Program.cs
+++ GameConsole/GameConsole/Program.cs
@@ -10,13 +10,16 @@ namespace GameConsole
     {
         static void Main(string[] args)
         {
-            PlayerCharacter player = new PlayerCharacter();
-            player.DaysSinceLastLogin = 42;
+            PlayerCharacter[] players = new PlayerCharacter[3]
+            {
+                new PlayerCharacter{ Name="Sarah"},
+                new PlayerCharacter(),
+                null
+            };
 
-            //int days = player.DaysSinceLastLogin.Value;
-            int days = player?.DaysSinceLastLogin ?? -1;
-            
-            Console.WriteLine(days);
+            string p1 = players?[0]?.Name;
+            string p2 = players?[1]?.Name;
+            string p3 = players?[2]?.Name;
             
             Console.ReadLine();
         }
