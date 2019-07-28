7d039bf76b755f35f6602c4cf5bf772084417a5f
Mohit Sharma <mohitsharmaonline@gmail.com>
2019-07-28 16:54:13 +0530

    Use of Null Coasceling operator again.

--- GameConsole/GameConsole/Program.cs
+++ GameConsole/GameConsole/Program.cs
@@ -13,7 +13,8 @@ namespace GameConsole
             var player = new PlayerCharacter();
             //player.DaysSinceLastLogin = 42;
 
-            int days = player.DaysSinceLastLogin.Value;
+            //int days = player.DaysSinceLastLogin.Value;
+            int days = player.DaysSinceLastLogin ?? -1;
             Console.WriteLine(days);
             
             Console.ReadLine();
